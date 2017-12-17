a81a65e1959a32f8aae6cd5f5129b80cd42f6d31
Qays_ <635216690@qq.com>
2017-12-17 16:25:19 +0800

    socket enhanced

--- src/test/java/ServerBeta.java
+++ src/test/java/ServerBeta.java
@@ -1,31 +1,49 @@
+import org.json.JSONObject;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
  * BIO
- *
+ * <p>
  * Created by Q-ays.
  * 12-16-2017 21:39
  */
 public class ServerBeta {
 
     //Stores socket and print writer of socket
-    static volatile  Map<Socket, PrintWriter> map = new ConcurrentHashMap<>();
+    static volatile Map<Socket, PrintWriter> map = new ConcurrentHashMap<>();
 
-    static class ThreadPool implements Runnable{
+    //Stores socket ip address
+    static volatile Map<String, Long> limitMap = new HashMap<>();
+
+    //password
+    static volatile String password = "";
+
+    static class ThreadPool implements Runnable {
         Socket socket = null;
 
         public ThreadPool(Socket socket) {
             this.socket = socket;
         }
 
+        public boolean verification(String pw) {
+            if (pw.equals(password)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         @Override
-        public void run(){
+        public void run() {
             System.out.println(Thread.currentThread().getName() + " connected!");
 
             try (InputStream is = socket.getInputStream();
@@ -34,17 +52,50 @@ public class ServerBeta {
                  OutputStream os = socket.getOutputStream();
                  PrintWriter pw = new PrintWriter(os)) {
 
+                String info;
+                String identity;
+
+                //todo verifies
+                for (int i = 0; ; i++) {
+
+                    pw.write("Enter password: \n");
+                    pw.flush();
+
+                    if ((info = br.readLine()) != null)
+                        if (verification(info)) {
+                            pw.write("Connect success\r\n");
+                            pw.write("Please input your name: \r\n");
+                            pw.flush();
+
+                            if ((identity = br.readLine()) == null) {
+                                identity = UUID.randomUUID().toString();
+                            }
+
+                            pw.write(identity + ", welcome to join\r\n");
+                            pw.write("You can type something here:\r\n");
+                            pw.flush();
+
+                            break;
+                        } else {
+                            pw.write("you type error password!\r\n");
+                            pw.flush();
+                            if (i > 2) {
+                                limitMap.put(socket.getInetAddress().toString(), System.currentTimeMillis());
+                                socket.close();
+                            }
+                        }
+                }
+
                 map.put(socket, pw);
                 System.out.println("current active thread: " + map.size());
 
-                String info;
                 while ((info = br.readLine()) != null) {
-                    System.out.println(socket.getInetAddress().toString() + ":" + socket.getPort() + ">>" + info);
+                    System.out.println(identity + "@" + socket.getInetAddress().toString() + ":" + socket.getPort() + ">>" + info);
 
                     for (Map.Entry<Socket, PrintWriter> entry : map.entrySet()) {
                         PrintWriter pw1 = entry.getValue();
 
-                        pw1.write(socket.getInetAddress().toString() + ":" + socket.getPort() + ">>" + info + "\n");
+                        pw1.write(identity + "@" + socket.getInetAddress().toString() + ":" + socket.getPort() + ">>" + info + "\n");
 
                         pw1.flush();
                     }
@@ -60,7 +111,7 @@ public class ServerBeta {
                 }
 
                 map.remove(socket); //removes socket from map
-                System.out.println("Remove this socket from map");
+                System.out.println("Remove this socket from map, sizes : " + map.size());
             }
         }
     }
@@ -68,6 +119,12 @@ public class ServerBeta {
     public static void main(String[] args) {
 
         int max = 5; //The max number of thread pool
+
+        int limitTime = 10 * 1000; //ms
+
+        if (args.length > 0) {
+            password = args[0];
+        }
 
         try {
             ServerSocket serverSocket = new ServerSocket(18888, 1);
@@ -79,7 +136,17 @@ public class ServerBeta {
 
                     Socket socket = serverSocket.accept();
 
-                    fixedThreadPool.execute(new ServerBeta.ThreadPool(socket));
+                    for (Map.Entry<String, Long> entry : limitMap.entrySet()) {
+                        if (entry.getKey().equals(socket.getInetAddress().toString())) {
+                            if (System.currentTimeMillis() < entry.getValue() + limitTime) {
+                                socket.close();
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!socket.isClosed())
+                        fixedThreadPool.execute(new ServerBeta.ThreadPool(socket));
                 }
             }
         } catch (IOException e) {
