c25035dd49b441b854628e48b2beb2f76affb7f8
qays <635216690@qq.com>
2018-02-05 17:25:52 +0800

    automatic generate mapper entity service ...

---
+++ src/test/java/CustomGenerator.java
@@ -0,0 +1,70 @@
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * Created by Q-ays.
+ * whosqays@gmail.com
+ * 02-05-2018 16:40
+ */
+public class CustomGenerator {
+    public static void main(String[] args) throws InterruptedException {
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir("E://data");
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);
+        gc.setEnableCache(true);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(true);// XML columList
+        gc.setAuthor("Q-ays");
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+//        gc.setMapperName("%sDao");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImp");
+//        gc.setControllerName("%sController");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        /*dsc.setTypeConvert(new MySqlTypeConvert(){
+            // 自定义数据库表字段类型转换【可选】
+            @Override
+            public DbColumnType processTypeConvert(String fieldType) {
+                System.out.println("转换类型：" + fieldType);
+                return super.processTypeConvert(fieldType);
+            }
+        });*/
+
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUrl("jdbc:mysql://localhost:3306/backend1?useUnicode=true&amp;characterEncoding=UTF-8&amp;generateSimpleParameterMetadata=true");
+        dsc.setUsername("root");
+        dsc.setPassword("Qays1234-");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
+        // strategy.setTablePrefix(new String[] { "tlog_", "tsys_" });// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+         strategy.setInclude(new String[] { "products" }); // 需要生成的表
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.shuwang");
+        pc.setModuleName("wbms");
+        mpg.setPackageInfo(pc);
+
+        // 执行生成
+        mpg.execute();
+    }
+}
