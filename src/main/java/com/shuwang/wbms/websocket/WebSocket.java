a7547a44b2ec70d92c27724ecc2c3bb6fd85061d
qays <635216690@qq.com>
2018-03-16 12:25:17 +0800

    add web socket by tomcat

---
+++ src/main/java/com/shuwang/wbms/websocket/WebSocket.java
@@ -0,0 +1,92 @@
+package com.shuwang.wbms.websocket;
+
+import org.json.JSONObject;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Q-ays.
+ * whosqays@gmail.com
+ * 03-16-2018 11:52
+ */
+@ServerEndpoint("/websocket/{username}")
+public class WebSocket {
+    private static int onlineCount = 0;
+    private static Map<String, WebSocket> clients = new ConcurrentHashMap<String, WebSocket>();
+    private Session session;
+    private String username;
+
+    @OnOpen
+    public void onOpen(@PathParam("username") String username, Session session) throws IOException {
+
+        this.username = username;
+        this.session = session;
+
+        addOnlineCount();
+        clients.put(username, this);
+        System.out.println("已连接");
+    }
+
+    @OnClose
+    public void onClose() throws IOException {
+        clients.remove(username);
+        subOnlineCount();
+    }
+
+    @OnMessage
+    public void onMessage(String message) throws IOException {
+
+        System.out.println(message);
+
+        JSONObject jsonTo = new JSONObject(message);
+
+        if (!jsonTo.get("To").equals("All")){
+            sendMessageTo("给一个人", jsonTo.get("To").toString());
+        }else{
+            sendMessageAll("给所有人");
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    public void sendMessageTo(String message, String To) throws IOException {
+        // session.getBasicRemote().sendText(message);
+        //session.getAsyncRemote().sendText(message);
+        for (WebSocket item : clients.values()) {
+            if (item.username.equals(To) )
+                item.session.getAsyncRemote().sendText(message);
+        }
+    }
+
+    public void sendMessageAll(String message) throws IOException {
+        for (WebSocket item : clients.values()) {
+            item.session.getAsyncRemote().sendText(message);
+        }
+    }
+
+
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        WebSocket.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        WebSocket.onlineCount--;
+    }
+
+    public static synchronized Map<String, WebSocket> getClients() {
+        return clients;
+    }
+}
